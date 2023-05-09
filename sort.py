4aaaa5548d220422b772a7f27d7fd8288c11e13e
AlexDarkVape <kalash23rusa@gmail.com>
2023-05-09 11:00:34 +0400

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,14 @@
+def get1ferst_words(input_string):
+    words = input_string.split()[:1]
+    first_two_words = " ".join(words)
+    return first_two_words
+
+def get2ferst_words(input_string):
+    words = input_string.split()[:2]
+    first_two_words = " ".join(words)
+    return first_two_words
+
+def get3ferst_words(input_string):
+    words = input_string.split()[:3]
+    first_two_words = " ".join(words)
+    return first_two_words
